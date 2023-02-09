ec747333245bd31d3430be60aa4181fdf7edd3b6
dm <d79123244656@yandex.ru>
2023-02-09 20:13:11 +0300

    Работа с формами

--- REST/frontend/src/components/Book.js
+++ REST/frontend/src/components/Book.js
@@ -1,32 +1,32 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
-const BookItem = ({item}) => {
+const BookItem = ({item, deleteBook}) => {
 return(
     <tr>
-        <td>
-            {item.name}
-        </td>
-        <td>
-            {item.author[0].first_name}
-        </td>
+        <td>{item.id}</td>
+        <td>{item.name}</td>
+        <td>{item.author[0].first_name}</td>
+        <td><button onClick={()=>deleteBook(item.id)} type='button'>Delete</button></td>
     </tr>
 )}
 
-const BookList = ({items}) => {
+const BookList = ({items, deleteBook}) => {
     return(
-        <table>
-            <thead>
-                <tr>
-                    <th>
-                        Name
-                    </th>
-                    <th>
-                        Author
-                    </th>
-                </tr>
-            </thead>
-            {items.map((item) => <BookItem item={item} />)}
-        </table>
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Author</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                {items.map((item) => <BookItem item={item} deleteBook={deleteBook} />)}
+            </table>
+        <Link to='/books/create'>Create</Link>
+        </div>
         )
 }
 
